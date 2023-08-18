9021457c828ffa9579b1c7b5ef4f38b27c895716
peter ji <hyeongjun9@gmail.com>
2023-08-18 15:38:50 -0700

    completed week 5 assessment

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -11,6 +11,17 @@ filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
 # Pseudo code:
+# Create a method that accepts an array and a letter as parameters
+# Using the select method, filter out words that include the letter
+
+def array_letter (arr, char)
+    arr.select do |word|
+        word.include?(char)
+    end
+end
+
+# p array_letter(beverages_array, filter_letter_o)
+# p array_letter(beverages_array, filter_letter_t)
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -19,12 +30,59 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+# Create a method that accepts a hash as parameter
+# Grab the values of hash using .values 
+# Use .flatten to get rid of nested array
+# Return array sorted using .sort
+
+def sorted_array hash
+    hash.values.flatten.sort
+end
+
+# p sorted_array(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 # Pseudo code:
+# Create a class called Bike
+# Create a initialize method with a model, wheels, and current_speed
+# Initialize value of wheels to 2
+# Initialize default value of 0 to current_speed
+# Use the attr_accessor and pass in the symbols before initialize
+# Create bike_info method
+# Return a sentence with all data from bike object
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+
+    def initialize(model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+
+    def bike_info
+        "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+    end
+
+    def pedal_faster num
+        @current_speed += num
+    end
+
+    def brake num
+        @current_speed -= num
+        if @current_speed < 0
+            @current_speed = 0
+        end
+        @current_speed
+    end
+
+end
+
+bike = Bike.new('Trek')
+p bike.bike_info
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -34,3 +92,14 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(25) => 0
 
 # Pseudo code:
+# Create a pedal_faster method inside Bike class
+# Pass in number as parameter
+# increment speed by the given number
+# Create a brake method inside Bike class
+# Pass in number as parameter
+# decrement speed by the given number
+# if speed is less than 0, assign speed to 0
+p bike.pedal_faster(10)
+p bike.pedal_faster(18)
+p bike.brake(5)
+p bike.brake(25)
